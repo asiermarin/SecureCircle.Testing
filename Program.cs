889846a969cc2d3100843a23375511220848b490
José Antonio Lorenzo Fernandez <j.lorenzo@lantek.com>
2021-11-30 09:59:14 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PruebaSecureCircle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
