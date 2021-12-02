1c7d2c789da5320b60c9e0910f4dc608558b6693
Asier Marín Fernández <A.marin@lantek.es>
2021-12-02 10:39:20 +0100

    Testing.

--- Program.cs
+++ Program.cs
@@ -7,10 +7,6 @@ namespace PruebaSecureCircle
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-
-
-
-
             Console.WriteLine();
         }
     }
